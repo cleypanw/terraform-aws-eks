63049d8e52ea119a817b1b584b3cfba7dcc5d093
chrisley75 <cley@paloaltonetworks.com>
2024-09-17 10:24:53 +0200

    Upgrade hashicorp/aws terraform provider to 5.67.0 and terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc to 5.44.0

--- terraform/aws_infra/eks-ebs-csi.tf
+++ terraform/aws_infra/eks-ebs-csi.tf
@@ -5,7 +5,7 @@ data "aws_iam_policy" "ebs_csi_policy" {
 
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+  version = "5.44.0"
 
   create_role                   = true
   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
