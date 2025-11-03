bbc53d6d4c4d08f24662a7ccf3187add6727df93
chrisley75 <cley@paloaltonetworks.com>
2025-11-03 16:17:18 +0100

    change perm

--- terraform/aws_infra/node-group-role.tf
+++ terraform/aws_infra/node-group-role.tf
@@ -1,4 +1,6 @@
-# IAM Role for EKS Node Group
+# node-group-role.tf
+# IAM Role for EKS Node Group, with all required policies including EC2 permissions for Launch Templates
+
 resource "aws_iam_role" "eks_node_group" {
   name = "${local.cluster_name}-node-group-role"
 
@@ -16,25 +18,54 @@ resource "aws_iam_role" "eks_node_group" {
   })
 }
 
-# Attach EKS Worker Node Policy
+# Attach standard EKS worker node policies
 resource "aws_iam_role_policy_attachment" "eks_worker_node" {
   role       = aws_iam_role.eks_node_group.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-# Attach EKS CNI Policy
 resource "aws_iam_role_policy_attachment" "eks_cni" {
   role       = aws_iam_role.eks_node_group.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-# Attach ECR ReadOnly Policy
 resource "aws_iam_role_policy_attachment" "eks_ecr_read" {
   role       = aws_iam_role.eks_node_group.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# Output ARN for use in EKS Node Group
+# Custom inline policy for EC2 permissions required for Launch Template
+resource "aws_iam_policy" "eks_node_group_ec2_permissions" {
+  name        = "${local.cluster_name}-eks-node-group-ec2"
+  description = "EC2 permissions required for EKS Node Group with Launch Template"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ec2:RunInstances",
+          "ec2:DescribeInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeVpcs",
+          "ec2:CreateTags",
+          "ec2:DescribeKeyPairs"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_ec2_attach" {
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = aws_iam_policy.eks_node_group_ec2_permissions.arn
+}
+
+# Output the ARN so eks-cluster.tf can reference it
 output "eks_node_group_arn" {
   value = aws_iam_role.eks_node_group.arn
 }
