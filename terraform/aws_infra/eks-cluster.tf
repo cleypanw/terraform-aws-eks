01b403a3c4f19ee0b4ca8ec2283773de3c255ac1
chrisley75 <cley@paloaltonetworks.com>
2025-11-03 13:32:06 +0100

    Update 1.32 and remove EBS

--- terraform/aws_infra/eks-cluster.tf
+++ terraform/aws_infra/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "20.26.0"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.32"
 
   vpc_id                                = module.vpc.vpc_id
   subnet_ids                            = module.vpc.private_subnets
