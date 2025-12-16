314d195ce549c03bf8f912978228aba2fed9994c
chrisley75 <cley@paloaltonetworks.com>
2025-12-16 14:57:41 +0100

    Change ami

--- terraform/aws_infra/main.tf
+++ terraform/aws_infra/main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-mantic-23.10-amd64-server-20240125"]
   }
   filter {
     name   = "virtualization-type"
